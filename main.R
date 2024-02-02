3f245dde46d60e04658f4fa2d1ba43882bec6666
storabi <smastorabi@gmail.com>
2024-02-02 18:36:19 +0100

    added year effect, year difference effect, ect

--- main.R
+++ main.R
@@ -16,7 +16,7 @@ calculate_RMSE <- function(y,y_hat){
   N <- length(y)
   sqrt((1/N)*sum((y - y_hat)^2))}
 
-calculate_mean_rating_per_gerne <- function(df, all_genres){
+calculate_mean_rating_per_genre <- function(df, all_genres){
   sapply(all_genres, function(my_genre){
     mean(df %>% filter(str_detect(genres, my_genre)) %>% 
            pull(rating))})}
@@ -24,7 +24,7 @@ calculate_mean_rating_per_gerne <- function(df, all_genres){
 get_genre_score <- function(df, df_genres){
   genre_combinations <- df$genre_list
   genre_score <- sapply(genre_combinations, function(my_combination){
-    movie_genre_score <- sum(
+    movie_genre_score <- mean(
       df_genres %>% 
         filter(genre_name %in% my_combination) %>% 
         pull(mean_rating))
@@ -38,7 +38,7 @@ load("edx.Rdata")
 # quickly see how the dataset looks like
 head(edx)
 
-#edx_downsized <- edx[sample(nrow(edx), size = 50000, replace = FALSE), ]
+edx_downsized <- edx[sample(nrow(edx), size = 50000, replace = FALSE), ]
 
 # see how many users and movies we have
 n_users <- n_distinct(edx$userId)
@@ -55,17 +55,6 @@ cat("number of movie ratings present in dataset: ", nrow(edx))
 cat("sparsity: ", 1 - nrow(edx)/(n_movies*n_users))
 
 # Let's do some EDA and see how does the data behave
-# calculate average of rating range (not average of ratings)
-range_average <- (max(edx$rating) - min(edx$rating))/2
-
-# proportion of ratings above the range average
-ratio_above_range_average <- mean(
-  edx %>% 
-  mutate(above_range_average = rating > range_average) %>%
-  pull(above_range_average))
-
-cat(glue("The average of the rating range is {range_average}"))
-cat(glue("About {round(ratio_above_range_average*100)}% of the ratings are above the range average"))
 
 # Visualize relationship between rating and frequency of rating for a movie
 rating_vs_number <- edx %>% 
@@ -78,11 +67,14 @@ joint_plot <- rating_vs_number %>%
   geom_point(alpha=0.15) + scale_x_continuous(trans="log10")
 ggExtra::ggMarginal(p = joint_plot, type = "histogram")
 
-rating_vs_number_corr <- cor(rating_vs_number$average_rating, rating_vs_number$number_of_ratings)
+rating_vs_number_corr <- cor(
+  rating_vs_number$average_rating, 
+  rating_vs_number$number_of_ratings)
+
 cat(glue("correlation coefficient between average rating and number of ratings is {rating_vs_number_corr}"))
 
-# So it makes sense to add number of ratings as a feature to our data set
-edx <- left_join(edx, rating_vs_number, by=c("movieId"))
+# So it may makes sense to add number of ratings as a feature to our data set
+edx <- left_join(edx, rating_vs_number, by=c("movieId")) # ==> data leakage?
 
 rm(rating_vs_number)
 
@@ -90,15 +82,16 @@ rm(rating_vs_number)
 cat(glue("There are {n_distinct(edx$genres)} distinct genres in the dataset"))
 
 # but each movie has a combination of genres. Let's see list of the core genres
-all_genres <- unique(unlist(str_split(edx$genres, "\\|"), recursive = FALSE))
 
 edx <- edx %>% 
   mutate(genre_list=str_split(
     genres, 
     "\\|"))
 
+all_genres <- unique(unlist(edx$genre_list))
+
 # now we can calculate the mean rating for movies that has each of the genres listed
-genre_means <- calculate_mean_rating_per_gerne(df=edx, all_genres=all_genres)
+genre_means <- calculate_mean_rating_per_genre(df=edx, all_genres=all_genres)
 
 df_mean_per_genre <- data.frame(
   genre_name = all_genres[-length(all_genres)], # remove the last element (corresponds to no_genre)
@@ -114,54 +107,85 @@ df_mean_per_genre %>%
 
 # compare best and worst performing genres
 my_genres <- c("Film-Noir", "Horror")
-edx %>% mutate(genre_1 = ifelse(str_detect(genres,my_genres[1]), my_genres[1], glue("No {my_genres[1]}")),
-               genre_2 = ifelse(str_detect(genres,my_genres[2]), my_genres[2], glue("No {my_genres[2]}"))) %>%
+edx %>% mutate(genre_1 = ifelse(str_detect(genres,my_genres[1]), 
+                                my_genres[1], 
+                                glue("No {my_genres[1]}")),
+               genre_2 = ifelse(str_detect(genres,my_genres[2]), 
+                                my_genres[2], 
+                                glue("No {my_genres[2]}"))) %>%
   select(average_rating, genre_1, genre_2) %>%
   distinct() %>% # keep unique rows only
   ggplot(aes(average_rating)) + geom_density() + facet_grid(genre_2~genre_1)
 
-
-
 # there seems to be a genre dependency on rating. So we keep genre as a feature
 # now let's look at the effect of user on the rating
+
 my_plot <- edx %>%
   group_by(userId) %>% 
   summarize(rating_num = n(),
             avg_rating = mean(rating)) %>%
   ggplot(aes(x=rating_num, y=avg_rating)) + 
-  geom_point(alpha=0.15) + scale_x_continuous(trans="log10")
-ggExtra::ggMarginal(p = my_plot, type = "histogram") 
+  geom_point(alpha=0.10) + scale_x_continuous(trans="log10")
+ggExtra::ggMarginal(p = my_plot, type = "histogram", bins = 50) 
 
 # There seems to be a user dependency, because different users have different average
-# ratings. However, the distribution of average ratings per user seems to be normal
-
-edx <- edx %>% rename(c("genre_list" = "genre_list.x"))
-
-df_edx_distinct_movies <- edx[c("movieId", "genre_list")] %>% distinct()
-
-df_edx_distinct_movies$genre_scores <- get_genre_score(df=df_edx_distinct_movies,
-                                                       df_genres=df_mean_per_genre)
-
-df_edx_distinct_movies$genre_scores[is.na(df_edx_distinct_movies$genre_scores)] <- mean(
-  df_edx_distinct_movies$genre_scores,na.rm=TRUE)
-
-df_edx_distinct_movies <- df_edx_distinct_movies %>%
-  mutate(genre_score_category = cut(
-    genre_scores,
-    breaks = seq(min(genre_scores), 
-                 max(genre_scores), 
-                 length=6),
-    labels = c("F", "D", "C", "B", "A")
-  ))
+# ratings. And a big majority of users have rated very few movies. However, the 
+# distribution of average ratings per user seems to be normal
 
 
-edx <- left_join(edx,
-                 df_edx_distinct_movies, 
-                 by = "movieId")
+# Extract year and month as possibly useful features
+edx$year <- year(as_datetime(edx$timestamp))
+edx$month <- month(as_datetime(edx$timestamp))
 
+edx %>% 
+  group_by(year) %>% 
+  summarize(mean_rating = mean(rating), 
+            n_rating = n()) %>%
+  ggplot(aes(x=year, y=mean_rating)) + 
+  geom_point(aes(size=n_rating)) + 
+  geom_line()
+
+edx %>% 
+  group_by(month) %>% 
+  summarize(mean_rating = mean(rating), 
+            n_rating = n()) %>%
+  ggplot(aes(x=month, y=mean_rating)) + 
+  geom_point(aes(size=n_rating)) + 
+  geom_line()
+
+# month effect seems to be minimal. It makes sense to not use it as a feature 
+# to avoid an over-fitting situation
+
+
+# for a careful eye, there is more information in the dataset. One can extract
+# the release year of the movies from the title
+
+edx <- edx %>% mutate(release_year = as.numeric(sub(".*\\((\\d+)\\)", "\\1", title)))
+
+edx %>% 
+  group_by(release_year) %>% 
+  summarize(mean_rating = mean(rating), 
+            n_rating = n()) %>%
+  ggplot(aes(x=release_year, y=mean_rating)) + 
+  geom_point(aes(size=n_rating)) + 
+  geom_line() + geom_smooth()
+
+edx <- edx %>% mutate(years_difference = year - release_year)
+
+edx %>% 
+  group_by(years_difference) %>% 
+  summarize(mean_rating = mean(rating),
+           n_rating = n()) %>%
+  ggplot(aes(x=years_difference, y=mean_rating)) + 
+  geom_point(aes(size=n_rating)) + 
+  geom_line() + geom_smooth()
+
+
+#################################################################################
 
 # Let's split the data set into training and test sets
 test_indices <- createDataPartition(edx$rating, times=1, p=0.2, list=FALSE)
+
 test_set <- edx[test_indices,]
 train_set <- edx[-test_indices,]
  
@@ -173,6 +197,14 @@ test_set <- test_set %>%
 # average of all ratings
 mu <- mean(train_set$rating)
 
+# use mu (mean) as the prediction for all ==> rating = mu + randomness (model 0)
+predictions <- test_set %>% 
+  mutate(pred = mu) %>%
+  pull(pred)
+
+calculate_RMSE(test_set$rating, predictions) # 1.059996
+
+
 # use b_i to represent movie effect ==> rating = mu + b_i + randomness (model 1)
 movie_effect <- train_set %>% 
   group_by(movieId) %>% 
@@ -180,6 +212,14 @@ movie_effect <- train_set %>%
 
 # add the obtained movie effects to the training set
 train_set <- left_join(train_set, movie_effect, by=c("movieId"))
+
+predictions <- test_set %>% 
+  left_join(movie_effect, by='movieId') %>%
+  mutate(pred = mu + b_i) %>%
+  pull(pred)
+
+calculate_RMSE(test_set$rating, predictions) # 0.9440672
+
 
 # use b_u to represent user effect ==> rating = mu + b_i + b_u + randomness (model 2)
 user_effect <- train_set %>% 
@@ -196,86 +236,133 @@ predictions <- test_set %>%
   mutate(pred = mu + b_i + b_u) %>%
   pull(pred)
 
-calculate_RMSE(test_set$rating, predictions)
+calculate_RMSE(test_set$rating, predictions) # 0.8661476
 
 # although we took into account the individual movie effect using b_i, however,
 # the movie effect might be due to several reasons (e.g. actors, producer, etc.)
 # genre can be one of these reasons, but let's see if we improve the model by 
-# adding a specific genre effect g_i
-
+# adding a specific genre effect b_g
+# ==> rating = mu + b_i + b_u + b_g + randomness (model 3)
 
 genre_effect <- train_set %>% 
-  group_by(genre_score_category) %>% 
-  summarize(g_i = mean(rating - mu - b_i - b_i))
+  group_by(genres) %>% 
+  summarize(b_g = mean(rating - mu - b_i - b_u))
 
-train_set <- left_join(train_set, genre_effect, by=c("genre_score_category"))
+train_set <- left_join(train_set, genre_effect, by=c("genres"))
 
 predictions <- test_set %>% 
   left_join(movie_effect, by='movieId') %>%
   left_join(user_effect, by='userId') %>%
-  left_join(genre_effect, by='genre_score_category') %>%
-  mutate(pred = mu + b_i + b_u + g_i) %>%
+  left_join(genre_effect, by='genres') %>%
+  mutate(pred = mu + b_i + b_u + b_g) %>%
   pull(pred)
 
-calculate_RMSE(test_set$rating, predictions)
+calculate_RMSE(test_set$rating, predictions) # 0.8658121
 
 
-# Ok adding the genre didn't help. Let's not consider it. but let's regularize
-# the movie effect, as it can be possible that a movie with small number of 
-# rating can have disproportionately large effect
+# Adding the genre didn't help a lot, just a bit. Let's try the effect of year. 
+# ==> rating = mu + b_i + b_u + b_g + b_y +randomness (model 3)
+year_effect <- train_set %>% 
+  group_by(year) %>% 
+  summarize(b_y = mean(rating - mu - b_i - b_u - b_g))
 
-# let's first optimize lambda for model 1, and then apply the optimized b_i's to
-# model 2
+train_set <- left_join(train_set, year_effect, by=c("year"))
 
-lambdas <- seq(1, 3, 0.1)
+predictions <- test_set %>% 
+  left_join(movie_effect, by='movieId') %>%
+  left_join(user_effect, by='userId') %>%
+  left_join(genre_effect, by='genres') %>%
+  left_join(year_effect, by='year') %>%
+  mutate(pred = mu + b_i + b_u + b_g + b_y) %>%
+  pull(pred)
+
+calculate_RMSE(test_set$rating, predictions) # 0.8657893
+
+
+# Adding the effect of year didn't introduce much change. Let's try to add the 
+# release year effect rating = mu + b_i + b_u + b_g + b_y + + b_rl + randomness (model 3)
+
+release_year_effect <- train_set %>% 
+  group_by(release_year) %>% 
+  summarize(b_rl = mean(rating - mu - b_i - b_u - b_g - b_y))
+
+train_set <- left_join(train_set, release_year_effect, by=c("release_year"))
+
+predictions <- test_set %>% 
+  left_join(movie_effect, by='movieId') %>%
+  left_join(user_effect, by='userId') %>%
+  left_join(genre_effect, by='genres') %>%
+  left_join(year_effect, by='year') %>%
+  left_join(release_year_effect, by='release_year') %>%
+  mutate(pred = mu + b_i + b_u + b_g + b_y + b_rl) %>%
+  pull(pred)
+
+calculate_RMSE(test_set$rating, predictions) # 0.8657893
+
+
+# Let's regularize all
+# to see if we can reduce the loss. It can be possible that a small number of 
+# observations related to a movie, user, etc. can have a disproportionately
+# big effect
+
+# Do a grid search to find the optimal lambda. Note that b_i, b_u, and b_g are
+# all more or less centered around 0. So we don't need to do an extra step of 
+# removing the mean to make sure we are "shrinking the values to zero" 
+# Add plots of the bi, bu, etc.
+
+train_set <- train_set %>% select(-any_of(c("b_i", "b_u", "b_g", "b_y", "b_rl")))
+
+lambdas <- seq(0, 15)
 
 RMSEs <- sapply(lambdas, function(lambda){
-    movie_effect <- train_set %>% 
+  
+  print(lambda)
+  
+  movie_effect <- train_set %>% 
     group_by(movieId) %>% 
     summarize(b_i = sum(rating - mu)/(lambda+n()))
-    
-    train_set <- subset(train_set, select = -b_i)
-    train_set <- left_join(train_set, movie_effect, by=c("movieId"))
-    
-    predictions <- test_set %>% 
-      left_join(movie_effect, by='movieId') %>%
-      mutate(pred = mu + b_i) %>%
-      pull(pred)
-    calculate_RMSE(test_set$rating, predictions)
-}
+  train_set <- left_join(train_set, movie_effect, by=c("movieId"))
+  
+  user_effect <- train_set %>% 
+    group_by(userId) %>% 
+    summarize(b_u = sum(rating - mu - b_i)/(lambda + n()))
+  train_set <- left_join(train_set, user_effect, by=c("userId"))
+  
+  year_difference_effect <- train_set %>% 
+    group_by(years_difference) %>% 
+    summarize(b_yd = sum(rating - mu - b_i - b_u)/(n()))
+  train_set <- left_join(train_set, year_difference_effect, by=c("years_difference"))
+  
+  rl_year_effect <- train_set %>% 
+    group_by(release_year) %>% 
+    summarize(b_rl = sum(rating - mu - b_i - b_u - b_yd)/(n()))
+  train_set <- left_join(train_set, rl_year_effect, by=c("release_year"))
+
+  
+  genre_effect <- train_set %>% 
+    group_by(genres) %>% 
+    summarize(b_g = sum(rating - mu - b_i - b_u - b_yd - b_rl)/(n()))
+  train_set <- left_join(train_set, genre_effect, by=c("genres"))
+  
+  
+  predictions <- test_set %>% 
+    left_join(movie_effect, by='movieId') %>%
+    left_join(user_effect, by='userId') %>%
+    left_join(year_difference_effect, by='years_difference') %>%
+    left_join(genre_effect, by='genres') %>%
+    left_join(rl_year_effect, by='release_year') %>%
+    mutate(pred = mu + b_i + b_u + b_yd + b_g + b_rl) %>%
+    pull(pred)
+  
+  train_set <- train_set %>% select(-any_of(c("b_i", "b_u", "b_g", "b_y", "b_rl", "b_yd")))
+  
+  calculate_RMSE(test_set$rating, predictions)
+  
+  }
 )
 
-# lambda = 2.1 minimized RMSE. Let's use it and use user effect to see how much 
-# it improves RMSE
+plot(lambdas, RMSEs) 
 
-lambda <- 2.1
+lambdas[which(RMSEs==min(RMSEs))]
 
-movie_effect <- train_set %>% 
-  group_by(movieId) %>% 
-  summarize(b_i = sum(rating - mu)/(lambda+n()))
-
-train_set <- subset(train_set, select = -b_i)
-train_set <- subset(train_set, select = -b_u)
-
-train_set <- left_join(train_set, movie_effect, by=c("movieId"))
-
-user_effect <- train_set %>% 
-  group_by(userId) %>% 
-  summarize(b_u = mean(rating - mu - b_i))
-
-train_set <- left_join(train_set, user_effect, by=c("userId"))
-
-predictions <- test_set %>% 
-  left_join(movie_effect, by='movieId') %>%
-  left_join(user_effect, by='userId') %>%
-  mutate(pred = mu + b_i + b_u) %>%
-  pull(pred)
-
-calculate_RMSE(test_set$rating, predictions)
-
-
-
-
-
-
-
+min(RMSEs) # 0.8638806
